beae64701e29ace1dfeda4bc0f9078677d2a8107
Liam Noone <liam@liamnoone.net>
2015-11-14 03:02:41 +0000

    Add empty CLI class

---
+++ lib/saves.rb
@@ -0,0 +1,10 @@
+module Saves
+  autoload :CLI, 'saves/cli'
+  autoload :Config, 'saves/config'
+
+  autoload :Backup, 'saves/backup'
+  autoload :Restore, 'saves/restore'
+
+  autoload :Game, 'saves/game'
+  autoload :GameList, 'saves/game_list'
+end
