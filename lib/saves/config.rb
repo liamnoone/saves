96dde5a5d33ca77cf71aba4d9ecea6bbc70d0601
Liam Noone <liam@liamnoone.net>
2015-11-14 03:10:05 +0000

    Add the Configuration code layer

---
+++ lib/saves/config.rb
@@ -0,0 +1,23 @@
+module Saves
+  class Config
+    class << self
+      def games_list
+        return @games_list if @games_list
+
+        games_list  = config_file('games.yml')
+        @games_list = YAML.load_file(games_list)
+      end
+
+      private
+
+      def config_dir
+        root_path = File.expand_path(File.join(__FILE__, '..', '..', '..'))
+        File.join(root_path, 'config')
+      end
+
+      def config_file(config_file)
+        File.expand_path File.join(config_dir, config_file)
+      end
+    end
+  end
+end
